0d6cdbfe9086dd0224c79679ff66ae1dc04abe48
DC <dcposch@dcpos.ch>
2022-08-21 12:19:30 -0700

    Deploy to testnet and verify

--- package/contracts/script/Deploy.s.sol
+++ package/contracts/script/Deploy.s.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/Counter.sol";
 
-contract CounterScript is Script {
+contract DeployScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        new Counter();
     }
 }
